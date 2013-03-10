a9e8111f8567829a31f2217cd9e531a6c57c0ac0
Akshay Bhardwaj <bhardwaj.akshay07@gmail.com>
2013-03-10 17:38:31 +0530

    Added gplv3 license to code files

--- krit.rkt
+++ krit.rkt
@@ -1,5 +1,19 @@
 ;; @Auther: Akshay Bhardwaj
-;; @Details: New programming language developed while undergoing a course
+;; @Details: New programming language developed while undergoing a programming course
+;; This file is part of Krit.
+
+;;    Foobar is free software: you can redistribute it and/or modify
+;;    it under the terms of the GNU General Public License as published by
+;;    the Free Software Foundation, either version 3 of the License, or
+;;    (at your option) any later version.
+
+;;    Foobar is distributed in the hope that it will be useful,
+;;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;    GNU General Public License for more details.
+
+;;    You should have received a copy of the GNU General Public License
+;;    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
 #lang racket
 (provide (all-defined-out)) ;; so we can put tests in a second file
 
